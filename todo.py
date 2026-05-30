5ab002eab15f957cc251bd3ddea9e23a29b26acf
Ayushi Kumari <ayushiisingh28@gmail.com>
2026-05-30 11:35:44 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,28 @@
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added!")
+
+    elif choice == "2":
+        print("\nTasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+    elif choice == "3":
+        num = int(input("Enter task number to delete: "))
+        tasks.pop(num - 1)
+        print("Task deleted!")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
